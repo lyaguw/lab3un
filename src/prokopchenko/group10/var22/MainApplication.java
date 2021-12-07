46c0f10e6495cd97103dbbe1bff2aed9b29f95d9
lyaguw <rct.prokopchVA1@bsu.by>
2021-12-07 13:06:47 +0300

    idc what to say

---
+++ src/prokopchenko/group10/var22/MainApplication.java
@@ -0,0 +1,27 @@
+package prokopchenko.group10.var22;
+
+import javax.swing.*;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("NO ARGUMENTS");
+            System.exit(-1);
+        }
+
+        double[] coefficients = new double[args.length];
+        int i = 0;
+        try {
+            for (String arg: args) {
+                coefficients[i++] = Double.parseDouble(arg);
+            }
+        } catch (NumberFormatException exception) {
+            System.out.println("WRONG ARGUMENTS");
+            System.exit(-2);
+        }
+
+        GornerTableFrame frame = new GornerTableFrame(coefficients);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
